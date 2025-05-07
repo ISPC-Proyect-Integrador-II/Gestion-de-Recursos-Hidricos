6b20d49577807a6f3255a7ca2973ba1d90ccf72b
vittoriodurigutti <vittodutti@gmail.com>
2025-05-07 11:52:08 -0300

    backend

--- C prototipo/prueba/Hidroponia/src/control/iluminacion.cpp
+++
@@ -1,9 +0,0 @@
-#include "control/iluminacion.h"
-#include <Arduino.h>
-
-void inicializarIluminacion() {
-  pinMode(RELE_ILUMINACION_PIN, OUTPUT);
-  digitalWrite(RELE_ILUMINACION_PIN, LOW);
-}
-
-
